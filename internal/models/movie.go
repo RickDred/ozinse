2db7a841664768ca74fa4a12fbb3cc5f135ddf8e
RickDred <newakkism@gmail.com>
2024-04-06 17:01:07 +0500

    fix: add gorm fields

--- internal/models/movie.go
+++ internal/models/movie.go
@@ -1,16 +1,17 @@
 package models
 
 type Movie struct {
-	ID          uint   `gorm:"primarykey"`
-	Logo        string `gorm:"not null"`
-	Title       string `gorm:"not null"`
-	Description string `gorm:"not null"`
-	Producer    string `gorm:"not null"`
-	Director    string `gorm:"not null"`
-	Year        string `gorm:"not null"`
-	Type        string `gorm:"not null"`
-	Seasons     int    `gorm:"not null"`
-	Episodes    int    `gorm:"not null"`
+	ID          uint     `gorm:"primarykey"`
+	Logo        string   `gorm:"not null"`
+	Title       string   `gorm:"not null"`
+	Description string   `gorm:"not null"`
+	Producer    string   `gorm:"not null"`
+	Director    string   `gorm:"not null"`
+	Year        string   `gorm:"not null"`
+	Type        string   `gorm:"not null"`
+	Tags        []string `gorm:"type:text[]"`
+	Seasons     int      `gorm:"not null"`
+	Episodes    int      `gorm:"not null"`
 	Minutes     int
 	Screenshots []string `gorm:"type:text[]"`
 }
